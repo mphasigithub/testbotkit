f155973b9944f3ce93c62a43b522a82563a664f1
mphasigithub <156188716+mphasigithub@users.noreply.github.com>
2024-01-17 17:37:24 +0530

    Update SimpleConversationalBot.js

--- SimpleConversationalBot.js
+++ SimpleConversationalBot.js
@@ -2,6 +2,8 @@ var botId = "st-f7326cc0-bbc0-5439-8e87-471c3a36c633";
 var botName = "Flight Search_Webhook";
 var sdk = require("./lib/sdk");
 var debug  = require('debug')("Agent");
+var mockServiceUrlTest = "https://samplenodekore.azurewebsites.net/places";
+var { makeHttpCall } = require("./makeHttpCall");
 
 /*
  * This is the most basic example of BotKit.
@@ -10,6 +12,23 @@ var debug  = require('debug')("Agent");
  *
  * We can either update the message, or chose to call one of 'sendBotMessage' or 'sendUserMessage'
  */
+
+function findhydPlaces(callback) {
+    return new Promise(function(resolve, reject) {
+        makeHttpCall(
+            'get',
+            mockServiceUrlTest
+        )
+        .then(function(res) {
+            sdk.sendBotMessage(res.data, callback);
+            resolve(res.data);
+
+        }).catch(function(err) {
+            return reject(err);
+        })
+    });
+}
+
 module.exports = {
     botId   : botId,
     botName : botName,
@@ -18,13 +37,24 @@ module.exports = {
         debug("in on_user_message %s ", requestId);
         if (data.message === "Namaste") {
             data.message = "Namaste Jaipal";
+            findhydPlaces(callback);
             //Sends back 'Hello' to user.
             return sdk.sendUserMessage(data, callback);
         } else if(data.message === "is this botkit?"){
             data.message = "Yes, you are chating with botkit";
             //Sends back 'Hello' to user.
             return sdk.sendUserMessage(data, callback);
-        } else if(!data.agent_transfer){
+        } else if(data.message === "Place to visit in hyderabad"){
+            data.message = `Top 5 attaractions in Hyderabad
+            1. Charminar
+            2. Salar Jung Museum
+            3. Golconda Fort
+            4. Ramoji Film City
+            5. Hussain Sagar Lake`;
+            //Sends back 'Hello' to user.
+           
+            return sdk.sendUserMessage(data, callback);
+        }else if(!data.agent_transfer){
             //Forward the message to bot
             return sdk.sendBotMessage(data, callback);
         } else {
