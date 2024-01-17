b7ef7b7c439548ef222178ce4a2a7449e9e9f2f2
mphasigithub <156188716+mphasigithub@users.noreply.github.com>
2024-01-17 13:14:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.start();
 //sdk.registerBot(require('./FindAFlight.js'));
 sdk.registerBot(require('./SimpleConversationalBot.js'));
 /* sdk.registerBot(require('./SimpleConversationalBotWithMultipleBotId.js'));*/
-sdk.registerBot(require('./GuessTheNumber.js'));
-/* sdk.registerBot(require('./BookACab.js'));
+/*sdk.registerBot(require('./GuessTheNumber.js'));
+sdk.registerBot(require('./BookACab.js'));
 sdk.registerBot(require('./OrderAPizza.js'));
 sdk.registerBot(require('./BotVariables.js'));
 sdk.registerBot(require('./LiveChat.js')); */
